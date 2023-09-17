a5eafa4f1878ba376b79672323fd4b0e16834ab4
HanDongHun <95519378+mrsuit0114@users.noreply.github.com>
2023-09-17 19:40:59 +0900

    enemycontroller
    random spawn and movement
    autumatic destroy

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float destroyLimit = 16f;
+    public float moveSpeed = 2f; // Adjust the speed as needed
+    private Vector3 currentDirection;
+
+    private void Start()
+    {
+        Destroy(gameObject, destroyLimit);
+        StartCoroutine(MoveRandomly());
+    }
+
+    private IEnumerator MoveRandomly()
+    {
+        while (true) // Repeat indefinitely
+        {
+            // Generate a random direction
+            float randomAngle = Random.Range(0f, 360f);
+            currentDirection = Quaternion.Euler(0f, -randomAngle, 0f) * Vector3.right;
+            Debug.Log(currentDirection);
+
+            // Move in the current direction for a random duration
+            float moveDuration = Random.Range(1f, 4f); // Adjust as needed
+            float startTime = Time.time;
+            float endTime = startTime + moveDuration;
+
+            while (Time.time < endTime)
+            {
+                // Move the object in the current direction
+                transform.Translate(currentDirection * moveSpeed * Time.deltaTime);
+
+                yield return null; // Yield until the next frame
+            }
+
+            yield return null; // Yield for one frame before generating a new direction
+        }
+    }
+}
