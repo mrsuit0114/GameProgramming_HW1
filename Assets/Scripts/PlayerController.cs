c8a192554dcfff366a83eb0fc43a7ceafd46493a
HanDongHun <95519378+mrsuit0114@users.noreply.github.com>
2023-10-01 01:36:11 +0900

    f

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private int hp = 10;
+    [SerializeField]
+    private float moveSpeed = 5;
+    [SerializeField]
+    private float turnSpeed = 45;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void InputMove()
+    {
+        if(Input.)
+    }
+
+}
